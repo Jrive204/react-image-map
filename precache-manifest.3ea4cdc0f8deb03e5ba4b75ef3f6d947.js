6de78c89c989ecb44d6be502791407d1c14a6078
qiuziz <zhu_anly@163.com>
2020-05-09 07:33:17 +0000

    Deploying to gh-pages from master ccdadc2522b1e404d4696ea864e20f4dbd2a4ed5

---
+++ precache-manifest.3ea4cdc0f8deb03e5ba4b75ef3f6d947.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80f5df2934764882fa6bcc2c67e8149a",
+    "url": "/react-image-map/index.html"
+  },
+  {
+    "revision": "e9f748d0a60d57f5938c",
+    "url": "/react-image-map/static/css/2.a8c61310.chunk.css"
+  },
+  {
+    "revision": "b01740bdbaabf1d181aa",
+    "url": "/react-image-map/static/css/main.02a6daaa.chunk.css"
+  },
+  {
+    "revision": "e9f748d0a60d57f5938c",
+    "url": "/react-image-map/static/js/2.a9f9530a.chunk.js"
+  },
+  {
+    "revision": "b01740bdbaabf1d181aa",
+    "url": "/react-image-map/static/js/main.54ddeb42.chunk.js"
+  },
+  {
+    "revision": "1d61556e2f5199d2af90",
+    "url": "/react-image-map/static/js/runtime~main.d1a4c38f.js"
+  },
+  {
+    "revision": "3373acbe81479b4ff628f2e6f1a6985d",
+    "url": "/react-image-map/static/media/example.3373acbe.png"
+  }
+]);
